0a4cc2e156f0e0e580fbeb8c8fd3a255f48b985b
Denis Golovin <denis.golovin@rwth-aachen.de>
2014-04-12 04:30:24 +0200

    server stuff

---
+++ server/ts_app/ts_app/server.js
@@ -0,0 +1,102 @@
+var io = require('socket.io').listen(3001);
+var distance = require('gps-distance');
+var mongodb = require('mongodb');
+
+var MongoClient = mongodb.MongoClient;
+var ObjectID = mongodb.ObjectID;
+
+var MAX_RECEIVER = 100;
+var clients = [];
+var msg_queue = [];
+var MAX_MSG_QUEUE_LEN = 30;
+var db;
+
+io.sockets.on('connection', function (socket) {
+    console.log('user connected:', socket.id);
+    socket.on('disconnect', function () {
+        var position = clients.indexOf(socket.id);
+        if (~position)
+            clients.splice(position, 1);
+    });
+
+    //socket.emit('news', { hello: 'world' });
+    socket.on('send_area_msg', function (msg) {
+        //console.log(data);
+        //socket.broadcast.emit('get_msg', data);
+        msg = JSON.parse(msg);
+        msg.timestamp = new Date().getTime();
+
+        msg_queue.push(msg);
+
+        if (msg_queue.length > MAX_MSG_QUEUE_LEN)
+            msg_queue.pop();
+
+        var near_clients = clients.filter(function (val) {
+            return distance([val.gps, clients[socket.id].gps]) <= clients[socket.id].max_dist;
+        });
+
+        if (MAX_RECEIVER < near_clients.length)
+            clients[socket.id].dec_max_dist();
+
+        near_clients.forEach(function (val) {
+            msg = JSON.stringify(msg);
+            io.sockets.socket(val.id).emit("get_msg", msg);
+        });
+    });
+
+    socket.on('update_profile', function (data) {
+        var profile = JSON.parse(data);
+        if (profile.id != undefined) {
+            var id = profile.id;
+            delete profile.id;
+            db.collection('test_insert').update({ _id: new ObjectID(id) }, { $set: profile }, { w: 1 }, function (err) {
+                if (err)
+                    console.warn(err.message);
+                else
+                    console.log('successfully updated profile.');
+            });
+        } else {
+            db.collection('test_insert').insert(profile, function (err, docs) {
+                console.log('successfully created profile.');
+                socket.emit('get_id', docs._id);
+                clients[socket.id].id = docs._id;
+            });
+        }
+    });
+
+    socket.on('update_location', function (location) {
+        location = JSON.parse(location);
+        console.log('update_location', location);
+        clients[socket.id] = new ChatUser(socket.id, location);
+        var temp = JSON.stringify(msg_queue);
+        socket.emit('get_msg_buffer', temp);
+    });
+});
+
+MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, db_local) {
+    if (err)
+        throw err;
+    db = db_local;
+});
+
+var ChatUser = (function () {
+    function ChatUser(id, gps) {
+        this.max_dist = 10;
+        this.id = id;
+        this.gps = gps;
+    }
+    ChatUser.prototype.dec_max_dist = function () {
+        this.max_dist = Math.ceil(this.max_dist * 0.7);
+    };
+    return ChatUser;
+})();
+
+var Message = (function () {
+    function Message(text, gps, timestamp) {
+        this.text = text;
+        this.gps = gps;
+        this.timestamp = timestamp;
+    }
+    return Message;
+})();
+//# sourceMappingURL=server.js.map
